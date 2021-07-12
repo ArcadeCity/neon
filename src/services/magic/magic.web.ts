080335099b56da62e81aa41eb5aa0e2c74da6363
Christopher David <chris@arcade.city>
2021-07-12 05:07:22 -0400

    swap in dummy app key

--- src/services/magic/magic.web.ts
+++ src/services/magic/magic.web.ts
@@ -1,7 +1,7 @@
 import { Magic as MagicSDK } from 'magic-sdk'
 import { SolanaExtension } from '@magic-ext/solana'
 
-export const magic = new MagicSDK('pk_live_4C679FE162F04FDC', {
+export const magic = new MagicSDK('pk_live_F04C3354AACC233B', {
   extensions: [
     new SolanaExtension({
       rpcUrl: 'https://api.mainnet-beta.solana.com',
